c23f93fd5d7d4792ad59b050dcaff9520dccf2af
Cloud <83776413+MelancholyCloud@users.noreply.github.com>
2021-09-04 22:41:30 -0700

    Update Bad_code.js

--- Bad_code.js
+++ Bad_code.js
@@ -1,3 +1,15 @@
-(function Bad_code () {
-
+(function __Bad_code () {
+  foo = true
+  bar = false
+  faz = true
+  foobar = true
+  System=(function(){
+    return Object.freeze(new(function(){
+      this.out=new(function(){
+        this.println=console.log
+      })()
+    })())
+  })()
+  System.out.println(new String("Hello"+' World'))
+  if (foo ? foo && !bar : bar && faz || !foo ? foobar : faz || foobar && !foo) console.log(new String("Hope" + " you"+" have"+" a"+" great"+' time!'))
 })()
