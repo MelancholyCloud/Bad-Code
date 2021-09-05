f446cd69709acc6eb7a201011ba6292cc121c800
Cloud <83776413+MelancholyCloud@users.noreply.github.com>
2021-09-04 22:51:01 -0700

    Update Bad_code.js

--- Bad_code.js
+++ Bad_code.js
@@ -1,8 +1,6 @@
 (function __Bad_code () {
   foo = true
   bar = false
-  faz = true
-  foobar = true
   System=(function(){
     return Object.freeze(new(function(){
       this.out=new(function(){
@@ -10,6 +8,23 @@
       })()
     })())
   })()
+  faz = true
+  foobar = true
   System.out.println(new String("Hello"+' World'))
-  if (foo ? foo && !bar : bar && faz || !foo ? foobar : faz || foobar && !foo) console.log(new String("Hope" + " you"+" have"+" a"+" great"+' time!'))
+  if(foo ? foo && !bar : bar && faz || !foo ? foobar : faz || foobar && !foo) System.out.println(new String("Hope" + " you"+" have"+" a"+" great"+' time!'))
+  if(faz == true){
+    if(bar == true){
+      (async function __Boop_function(){
+        System.out.println(new String('Boop'))
+      })()
+		}else if(true == foobar){
+      (async function __Boop_function(){
+        System.out.println(new String('Boop'))
+      })()
+    }else{
+      (async function __Poob_function(){
+        System.out.println(new String('Poob'))
+      })()
+		}
+  }
 })()
