0626d983abe8e617dbdfc867b1b9bcfecc166c50
Cloud <83776413+MelancholyCloud@users.noreply.github.com>
2021-09-04 22:54:02 -0700

    Update Bad_code.js

--- Bad_code.js
+++ Bad_code.js
@@ -1,30 +1,31 @@
-(function __Bad_code () {
-  foo = true
-  bar = false
-  System=(function(){
-    return Object.freeze(new(function(){
-      this.out=new(function(){
-        this.println=console.log
-      })()
-    })())
-  })()
-  faz = true
-  foobar = true
-  System.out.println(new String("Hello"+' World'))
-  if(foo ? foo && !bar : bar && faz || !foo ? foobar : faz || foobar && !foo) System.out.println(new String("Hope" + " you"+" have"+" a"+" great"+' time!'))
-  if(faz == true){
-    if(bar == true){
-      (async function __Boop_function(){
-        System.out.println(new String('Boop'))
-      })()
-    }else if(true == foobar){
-      (async function __Boop_function(){
-        System.out.println(new String('Boop'))
-      })()
-    }else{
-      (async function __Poob_function(){
-        System.out.println(new String('Poob'))
-      })()
+(function __Very () {
+  (function __Bad_code () {
+    foo = true
+    bar = false
+    System=(function(){
+      return Object.freeze(new(function(){
+        this.out=new(function(){
+          this.println=console.log
+        })()
+      })())
+    })()
+    faz = true
+    foobar = true
+    System.out.println(new String("Hello"+' World'))
+    if(faz == true){
+      if(foo ? foo && !bar : bar && faz || !foo ? foobar : faz || foobar && !foo == true){
+        (async function __Boop_function(){
+          System.out.println(new String("Hope" + " you"+" have"+" a"+" great"+' time!'))
+        })()
+      }else if(true == foobar){
+        (async function __Boop_function(){
+          System.out.println(new String("Hope" + " you"+" have"+" a"+" great"+' time!'))
+        })()
+      }else{
+        (async function __Poob_function(){
+          System.out.println(new String("Hope" + " you"+" have"+" a"+" great"+' time!'))
+        })()
+      }
     }
-  }
+  })()
 })()
